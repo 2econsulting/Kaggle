092bf80c7ad4d334ba3b67db6566bca020704418
DS-hsw <heosungwook@2e.co.kr>
2018-07-04 15:08:17 +0900

    LightGBM in python (preprocessing -> tuning -> predict with cross-validation)

---
+++ Python/prepLGB.py
@@ -0,0 +1,21 @@
+# author: Heo Sung Wook
+
+import lightgbm as lgb
+
+def prepLGB(data, target):
+    labels = data[target]
+    
+    # Create Light GBM data set        
+    lData = lgb.Dataset(data = data.drop(target, axis = 1), 
+                        label = labels,
+                        feature_name = list(data.columns.drop(target)),
+                        categorical_feature = 'auto')
+    
+    return lData, labels
+
+""" 
+****** example ******
+
+trainDataL, labels = prepLGB(data = train, 
+                             target = "Survived")
+"""
