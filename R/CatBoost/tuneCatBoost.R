e8e39ecd660e1103da61da9b311f0c7f36de1fb7
DS-hsw <heosungwook@2e.co.kr>
2018-07-09 15:48:28 +0900

    tuneCatBoost Fix

--- R/CatBoost/tuneCatBoost.R
+++ R/CatBoost/tuneCatBoost.R
@@ -9,14 +9,12 @@ tuneCatBoost <- function(data, y, max_models, cv, gridtype="small-size"){
   
   if(gridtype=="small-size"){
     grid <- expand.grid(
-      # depth = c(3, 4, 5, 7, 9, 11, 13),
-      # learning_rate = c(0.01, 0.03),
-      depth = c(3,4,5,6),
-      learning_rate = c(0.1,0.3),
+      depth = c(3, 4, 5, 7, 9, 11, 13),
+      learning_rate = c(0.01, 0.03),
       l2_leaf_reg = 3,
       rsm = c(0.9,1),
       border_count = 32,
-      iterations = 100
+      iterations = 500
     )
   } else{
     grid <- expand.grid(
@@ -30,6 +28,66 @@ tuneCatBoost <- function(data, y, max_models, cv, gridtype="small-size"){
   }
   
   cat <- catboost.caret
+  
+  # cat$fit
+  cat$fit <- function (x, y, wts, param, lev, last, weights, classProbs, ...) 
+  {
+    param <- c(param, list(...), list(random_seed=1234))
+    if (is.null(param$loss_function)) {
+      param$loss_function = "RMSE"
+      if (is.factor(y)) {
+        param$loss_function = "Logloss"
+        if (length(lev) > 2) {
+          param$loss_function = "MultiClass"
+        }
+        y = as.double(y) - 1
+      }
+    }
+    pool <- catboost.load_pool(x, y, weight = wts)
+    model <- catboost.train(pool, NULL, param)
+    model$lev <- lev
+    return(model)
+  }
+  
+  # cat$prob
+  cat$prob <- 
+  function (modelFit, newdata, preProc = NULL, submodels = NULL) 
+  {
+    pool <- catboost.load_pool(newdata)
+    prediction <- catboost.predict(modelFit, pool, prediction_type = "Probability")
+    if (is.matrix(prediction)) {
+      colnames(prediction) <- modelFit$lev
+      prediction <- as.data.frame(prediction)
+    }
+    param <- catboost.get_model_params(modelFit)
+    if (param$loss_function$'type' == "Logloss") {
+      prediction <- cbind(1 - prediction, prediction)
+      colnames(prediction) <- modelFit$lev
+    }
+    if (!is.null(submodels)) {
+      tmp <- vector(mode = "list", length = nrow(submodels) + 
+                      1)
+      tmp[[1]] <- prediction
+      for (j in seq(along = submodels$iterations)) {
+        tmp_pred <- catboost.predict(modelFit, pool, prediction_type = "Probability", 
+                                     ntree_end = submodels$iterations[j])
+        if (is.matrix(tmp_pred)) {
+          colnames(tmp_pred) <- modelFit$lev
+          tmp_pred <- as.data.frame(tmp_pred)
+        }
+        param <- catboost.get_model_params(modelFit)
+        if (param$loss_function$'type' == "Logloss") {
+          tmp_pred <- cbind(1 - tmp_pred, tmp_pred)
+          colnames(tmp_pred) <- modelFit$lev
+        }
+        tmp[[j + 1]] <- tmp_pred
+      }
+      prediction <- tmp
+    }
+    return(prediction)
+  }
+  
+  # cat$type
   cat$type <- "Classification"
   
   setSeeds <- function(cv,num_grid) {
@@ -44,7 +102,7 @@ tuneCatBoost <- function(data, y, max_models, cv, gridtype="small-size"){
   set.seed(1234)
   fit_control <- caret::trainControl(
     method = "repeatedcv", 
-    number = cv, 
+    number = cv,
     classProbs = TRUE,
     seeds = setSeeds(cv=cv, num_grid=nrow(grid))
   )
